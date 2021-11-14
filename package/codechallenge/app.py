632102403c63095df7630d073561ca90e8556249
Pittalis Marco <pittalismarco@gmail.com>
2021-11-14 19:44:10 +0100

    Added Request.Session Factory

--- package/codechallenge/app.py
+++ package/codechallenge/app.py
@@ -1,9 +1,11 @@
 from pyramid.config import Configurator
 from pyramid.response import Response
+from pyramid.session import SignedCookieSessionFactory
 
 
 def main(global_config, **settings):
-    config = Configurator(settings=settings)
+    session_factory = SignedCookieSessionFactory('sessionFactory')
+    config = Configurator(settings=settings, session_factory=session_factory)
     config.include('pyramid_jinja2')    
     config.add_route('start', '/')
     config.add_route('question', '/question')
