95de84aa2ccbe4bc9e4c4e106d6f3b4e3a573945
Pittalis Marco <pittalismarco@gmail.com>
2021-11-30 22:14:53 +0100

    Added naming convention

--- package/codechallenge/models/meta.py
+++ package/codechallenge/models/meta.py
@@ -4,10 +4,17 @@ from sqlalchemy import Column, Integer, String, select, engine_from_config
 from sqlalchemy.orm import declarative_base, Session, sessionmaker
 
 
+NAMING_CONVENTION = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
 
-# TODO: replace with from functools import lru_cache
 cache = {}
 Base = declarative_base()
+Base.metadata.naming_convention = NAMING_CONVENTION
 
 
 def get_engine(settings, prefix='sqlalchemy.'):
