3e34088a16d90f4d8b1821dbfd0c25bfa24a4d49
Pittalis Marco <pittalismarco@gmail.com>
2021-12-08 17:29:39 +0100

    Moved integration tests to the proper module

--- package/codechallenge/tests/unit_tests.py
+++ package/codechallenge/tests/unit_tests.py
@@ -2,9 +2,6 @@ import pytest
 from codechallenge.app import StoreConfig
 from codechallenge.db import count
 from codechallenge.models import Answer, Question, User
-from codechallenge.views.views import CodeChallengeViews
-from pyramid import testing
-from pyramid.httpexceptions import HTTPBadRequest, HTTPSeeOther
 from sqlalchemy.exc import IntegrityError, InvalidRequestError
 
 
@@ -61,90 +58,3 @@ class TestCaseModels:
         new_user.set_password("password")
         new_user.save()
         assert new_user.check_password("password")
-
-
-class TestCaseTutorialView:
-    def t_start_view(self):
-        request = testing.DummyRequest()
-        view_obj = CodeChallengeViews(request)
-        response = view_obj.start()
-        assert response == {}
-
-    def t_textCodeAndPositionAreReturnedWhenQuestionIsFound(self, fillTestingDB):
-        request = testing.DummyRequest()
-        request.params.update(index=2)
-        view_obj = CodeChallengeViews(request)
-        response = view_obj.question()
-        assert response == {"text": "q2.text", "code": "q2.code", "position": 2}
-
-    def t_whenQuestionIsNoneEmptyDictIsReturned(self, fillTestingDB):
-        request = testing.DummyRequest()
-        request.params.update(index=30)
-        view_obj = CodeChallengeViews(request)
-        response = view_obj.question()
-        assert response == {}
-
-    def t_createNewQuestion(self, dummy_request, mocker):
-        mocker.patch("pyramid.testing.DummyRequest.is_authenticated")
-        request = dummy_request
-        request.json = {
-            "text": "eleven pm",
-            "code": "x = 0; x += 1; print(x)",
-            "position": 2,
-        }
-        view_obj = CodeChallengeViews(request)
-        response = view_obj.new_question()
-        assert count(Question) == 1
-        assert response["text"] == "eleven pm"
-        assert response["position"] == 2
-
-
-class TestCaseLoginRequired:
-    def t_checkViewsAreDecorated(self, dummy_request, simple_config):
-        view_obj = CodeChallengeViews(dummy_request)
-        for view_name in ["new_question", "edit_question"]:
-            view_method = getattr(view_obj, view_name)
-            response = view_method()
-            assert isinstance(response, HTTPSeeOther)
-
-
-class TestCaseLogin:
-    def t_retrieveLoginPage(self, dummy_request, simple_config):
-        next_url = "new_question"
-        view_obj = CodeChallengeViews(dummy_request)
-        response = view_obj.login()
-        assert response["next_url"].endswith(next_url)
-        assert response["url"].endswith("login")
-
-    def t_failedLoginAttempt(self, dummy_request, simple_config):
-        request = dummy_request
-        request.method = "POST"
-        request.params = {
-            "email": "user@test.com",
-            "password": "p@ss",
-        }
-        view_obj = CodeChallengeViews(request)
-        with pytest.raises(HTTPBadRequest):
-            view_obj.login()
-
-    def t_successfulLogin(self, dummy_request, simple_config):
-        credentials = {
-            "email": "user@test.com",
-            "password": "p@ss",
-        }
-        User(**credentials).create()
-        request = dummy_request
-        request.method = "POST"
-        request.params = credentials
-        view_obj = CodeChallengeViews(request)
-        response = view_obj.login()
-        assert isinstance(response, HTTPSeeOther)
-
-
-class TestCaseLogOut:
-    def t_successfulLogout(self, dummy_request, simple_config):
-        request = dummy_request
-        request.method = "POST"
-        view_obj = CodeChallengeViews(request)
-        response = view_obj.logout()
-        assert isinstance(response, HTTPSeeOther)
