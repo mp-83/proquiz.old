1063cc8325923b3a791231db6079080a15816d0c
Pittalis Marco <pittalismarco@gmail.com>
2022-02-14 21:14:55 +0100

    Refactored and simplified GameFactory and QuestionFactory

--- be/package/codechallenge/play/single_player.py
+++ be/package/codechallenge/play/single_player.py
@@ -15,10 +15,11 @@ class PlayException(Exception):
 class QuestionFactory:
     def __init__(self, game, counter=0):
         self._game = game
-        self._counter = counter
+        self._counter = counter - 1
         self._question = None
 
     def next_question(self):
+        self._counter += 1
         if (not self._game.questions and self._counter == 0) or (
             self._counter == len(self._game.questions)
         ):
@@ -29,7 +30,6 @@ class QuestionFactory:
         else:
             self._question = self._game.questions[self._counter]
 
-        self._counter += 1
         return self._question
 
     @property
@@ -42,14 +42,17 @@ class QuestionFactory:
 
     @property
     def is_last_question(self):
-        return self._counter == len(self._game.questions)
+        return self._counter == len(self._game.questions) - 1
 
 
 class GameFactory:
-    def __init__(self, match):
+    def __init__(self, match, counter=0):
         self._match = match
-        self._counter = -1
-        self._game = None
+        self._counter = counter - 1
+        if self._match.order:
+            self._games = self._match.ordered_games
+        else:
+            self._games = self._match.games
 
     @property
     def counter(self):
@@ -57,28 +60,25 @@ class GameFactory:
 
     @property
     def games(self):
-        if self._match.order:
-            return self._match.ordered_games
-        return self._match.games
+        return self._games
 
     def next_game(self):
-        if not self.games:
+        if not self._games:
             raise EmptyMatchError(f"Match {self._match.name} contains no Game")
 
         if self._counter == len(self.games) - 1:
             raise MatchOver(f"Match {self._match.name}")
 
         self._counter += 1
-        return self.current
+        return self.games[self._counter]
 
     @property
     def current(self):
-        self._game = self.games[self._counter]
-        return self._game
+        return self.games[self._counter]
 
     @property
     def match_started(self):
-        return self._game is not None
+        return self._counter > 0
 
     @property
     def is_last_game(self):
@@ -89,8 +89,7 @@ class SinglePlayer:
     def __init__(self, user, match):
         self._user = user
         self._current_match = match
-        self._current_game = None
-        self._game_factory = GameFactory(match)
+        self._game_factory = None
         self._question_factory = None
         self._current_reaction = None
 
@@ -101,24 +100,24 @@ class SinglePlayer:
                 f"User {self._user} has no left attempts for Match {self._current_match.name}"
             )
 
-        self._current_game = self.next_game()
-        self._question_factory = QuestionFactory(self.current_game)
+        self._game_factory = GameFactory(self._current_match)
+        _game = self.next_game()
+        self._question_factory = QuestionFactory(_game)
         question = self.next_question()
+
+        # left as sanity check. To be removed after testing
+        assert _game.uid == question.game.uid
+
         self._current_reaction = Reaction(
             match_uid=self._current_match.uid,
             question_uid=question.uid,
             user_uid=self._user.uid,
-            game_uid=self._current_game.uid,
+            game_uid=_game.uid,
+            q_counter=0,
+            g_counter=0,
         ).create()
 
-        # left as sanity check. To be removed after testing
-        assert self._current_game.uid == question.game.uid
-
         return question
-
-    @property
-    def current_question(self):
-        return self._question_factory.current
 
     @property
     def match_started(self):
@@ -127,6 +126,9 @@ class SinglePlayer:
     @property
     def current_game(self):
         return self._game_factory.current
+
+    def next_game(self):
+        return self._game_factory.next_game()
 
     @property
     def can_be_resumed(self):
@@ -167,6 +169,10 @@ class SinglePlayer:
         _question = self.next_question()
         return _question
 
+    @property
+    def current_question(self):
+        return self._question_factory.current
+
     def next_question(self):
         if not self.current_game:
             raise GameError(f"Match {self._current_match.name} is not started")
@@ -175,6 +181,3 @@ class SinglePlayer:
             return self._question_factory.next_question()
         except GameOver:
             self._current_game = self.next_game()
-
-    def next_game(self):
-        return self._game_factory.next_game()
